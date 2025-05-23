c3acf713048aa78e1a8b0a58576bc7ce0753edac
mehriddinsaidov <mehriddinsaidovl8@gmail.com>
2025-05-23 18:08:32 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+     extend: {},
+     screens:{
+      'md':{max:'510px'}
+     }
+   },
+   plugins: [],
+ }
